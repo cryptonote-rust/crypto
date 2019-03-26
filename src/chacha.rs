1b6346a8ab5ad0f1301798ed45759898c77b52ef
calidion <calidion@gmail.com>
2019-03-27 00:25:38 +0800

    removed println

--- src/chacha.rs
+++ src/chacha.rs
@@ -21,8 +21,6 @@ pub fn generate(password: String) -> ChachaKey {
   let aes = aes::new(AESSupport::HW);
   let input = password.as_bytes();
   let cn_hash = hash::hash_alloc_scratchpad(&input[0..], &aes, hash::HashVersion::Version6);
-  println!("input = {:x?}", input);
-  println!("hash = {}", cn_hash);
   let (data, _) = cn_hash.as_bytes().split_at(CHACHA_KEY_SIZE);
   assert!(data.len() == CHACHA_KEY_SIZE);
   let data: [u8; CHACHA_KEY_SIZE] = data.try_into().unwrap();
@@ -40,9 +38,6 @@ mod tests {
     let key = generate(String::from(""));
     assert!(key.data == [101, 98, 49, 52, 101, 56, 97, 56, 51, 51, 102, 97, 99, 54, 102, 101, 57, 97, 52, 51, 98, 53, 55, 98, 51, 51, 54, 55, 56, 57, 99, 52]);
     let key1 = generate(String::from("This is a test"));
-
     assert!(key1.data == [97, 48, 56, 52, 102, 48, 49, 100, 49, 52, 51, 55, 97, 48, 57, 99, 54, 57, 56, 53, 52, 48, 49, 98, 54, 48, 100, 52, 51, 53, 53, 52]);
-    println!("{:?}", key.data);
-    println!("{:?}", key1.data);
   }
 }
